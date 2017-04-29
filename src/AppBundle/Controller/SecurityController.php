85658699c7114f4f0d91cdad0e2a442e25d7ef6b
RAYCHEV <nikolay.g.raychev@gmail.com>
2017-04-29 10:37:33 +0100

    login method has been improved

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -15,6 +15,27 @@ class SecurityController extends Controller
      */
     public function login()
     {
-        return $this->render('security/login.html.twig');
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig',[
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+        var_dump("OK");
+    }
+
+
 }
